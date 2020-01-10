ed8761a870c86eaa7a9d7a3e9116f322da814ac6
SuruchBoss <Bossxiii@gmail.com>
2020-01-10 11:44:11 +0700

    Done

--- lib/log/HomePage.dart
+++ lib/log/HomePage.dart
@@ -14,6 +14,17 @@ import 'package:morpheus/morpheus.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 
 class HomePage extends StatefulWidget {
+  final int currentIndex;
+  final String teamName;
+  final String teamLevel;
+  final String teamAddr;
+  HomePage(
+      {Key key,
+      this.currentIndex,
+      this.teamName,
+      this.teamAddr,
+      this.teamLevel})
+      : super(key: key);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -24,6 +35,7 @@ class _HomePageState extends State<HomePage> {
   FirebaseUser user;
   bool isSignedIn = false;
   String imageUrl;
+  int _currentIndex;
 
   checkAuthentication() async {
     _auth.onAuthStateChanged.listen((user) {
@@ -54,80 +66,83 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    print("HomePage");
+    widget.currentIndex == null
+        ? _currentIndex = 0
+        : _currentIndex = widget.currentIndex;
+ 
     super.initState();
     this.checkAuthentication();
     this.getUser();
   }
 
-  final List<Widget> _screens = [
-    OneTab(),
-    TwoTab(),
-    SevenTab(),
-    ThreeTab(),
-    Feedx(),
-    HomeScreen(),
-  ];
-  int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
+    List<Widget> _screens = [
+      OneTab(),
+      TwoTab(
+          name: widget.teamName,
+          address: widget.teamAddr,
+          level: widget.teamLevel),
+      SevenTab(),
+      ThreeTab(),
+      Feedx(),
+      HomeScreen(),
+    ];
+
     print("building Feed");
     return Scaffold(
-      body: MorpheusTabView(child: _screens[_currentIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white.withOpacity(0.8),
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.black.withOpacity(0.7),
-        type: BottomNavigationBarType.fixed,
-        selectedFontSize: 10,
-        unselectedFontSize: 7,
-        iconSize: 30,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.user,
-            ),
-            title: Text('Profile'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.futbol_o,
-            ),
-            title: Text('MyTeam'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.group,
-            ),
-            title: Text('All Player'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.globe,
-            ),
-            title: Text('All Team'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.cloud,
-            ),
-            title: Text('Social'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              LineAwesomeIcons.hand_o_right,
-            ),
-            title: Text('News'),
-          ),
-        ],
-        onTap: (index) {
-          if (index != _currentIndex) {
-            setState(() => _currentIndex = index);
-          }
-        },
-      ),
-    );
+        body: MorpheusTabView(child: _screens[_currentIndex]),
+        bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.white.withOpacity(0.8),
+            currentIndex: _currentIndex,
+            selectedItemColor: Colors.green,
+            unselectedItemColor: Colors.black.withOpacity(0.7),
+            type: BottomNavigationBarType.fixed,
+            selectedFontSize: 10,
+            unselectedFontSize: 7,
+            iconSize: 30,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.user,
+                ),
+                title: Text('Profile'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.futbol_o,
+                ),
+                title: Text('MyTeam'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.group,
+                ),
+                title: Text('All Player'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.globe,
+                ),
+                title: Text('All Team'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.cloud,
+                ),
+                title: Text('Social'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  LineAwesomeIcons.hand_o_right,
+                ),
+                title: Text('News'),
+              ),
+            ],
+            onTap: (index) {
+              if (index != _currentIndex) {
+                setState(() => _currentIndex = index);
+              }
+            }));
   }
 }
